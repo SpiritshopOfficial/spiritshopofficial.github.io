a8b8327fff93a336147217b124f19ccedab39551
Spiritshop Official <112664001+SpiritshopOfficial@users.noreply.github.com>
2022-09-24 14:13:18 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ var isInstagram = navigator.userAgent.match(/instagram/i);
 if (isInstagram) {
   document.querySelector('html').classList.add("html-insta");
 }
+
+const d = new Date();
+let year = d.getFullYear();
+document.getElementById("year").innerHTML = year;

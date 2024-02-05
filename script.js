d8426a54875739eb96d4949525664ba8ab810fca
Spiritshop Official <112664001+SpiritshopOfficial@users.noreply.github.com>
2024-02-05 14:13:06 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,56 +1,68 @@
+let menu;
+let wrapper;
+
+/* Instagram Browser */
+
 var isInstagram = navigator.userAgent.match(/instagram/i);
 if (isInstagram) {
   document.querySelector('html').classList.add("html-insta");
 }
 
-/* SFM Planner */
+/* Scroll to Top */
+
+function scrollToTop() {
+  const wrapper = document.querySelector('.wrapper');
+
+  if (wrapper) {
+    wrapper.scrollTo({
+      top: 0,
+      behavior: 'smooth'
+    });
+  }
+}
+
+
+/* Copyright Date */
 
 const y = new Date();
 let year = y.getFullYear();
 document.getElementById("year").innerHTML = year;
 
-const display = ["JAN","FEB","MAR","APR","MAY","JUN","JUL","AUG","SEP","OCT","NOV","DEC"];
+/* Menu */
 
-const dates = [
-  new Date("2023-05-05"),
-  new Date("2023-06-04"),
-  new Date("2023-07-03"),
-  new Date("2023-08-01"),
-  new Date("2023-08-31"),
-  new Date("2023-09-29"),
-  new Date("2023-10-28"),
-  new Date("2023-11-27"),
-  new Date("2023-12-27"),
-  new Date("2024-01-25"),
-  new Date("2024-02-24"),
-  new Date("2024-03-25"),
-  new Date("2024-04-24"),
-  new Date("2024-05-23"),
-  new Date("2024-06-22"),
-  new Date("2024-07-21"),
-  new Date("2024-08-19"),
-  new Date("2024-09-18"),
-  new Date("2024-10-17"),
-  new Date("2024-11-15")
-];
+menu = document.querySelector(".menu");
+wrapper = document.querySelector(".wrapper");
 
-const currentDate = new Date();
-currentDate.setDate(currentDate.getDate() - 1);
-
-let index = 0;
-for (let i = 0; i < dates.length; i++) {
-  if (currentDate > dates[i]) {
-    index = i + 1;
+wrapper.onscroll = function () {
+  var currentScroll = wrapper.scrollTop; // Use wrapper's scrollTop
+  // Check the scroll position
+  if (currentScroll > 120) {
+    // If scrolled down, remove the 'hidden' class from the menu
+    menu.classList.add("visible");
   } else {
-    break;
+    // If at the top, add the 'hidden' class
+    menu.classList.remove("visible");
+  }
+};
+
+function toggleMenu() {
+  const body = document.body;
+  const menubutton = document.querySelector(".menu-button");
+
+  // Toggle the 'open' class on the menu
+  menu.classList.toggle("open");
+
+  // Toggle the 'clicked' class on the menu button
+  menubutton.classList.toggle("clicked");
+
+  // Check if the menu is open
+  if (menu.classList.contains("open")) {
+    // Disable scrolling
+    wrapper.style.overflow = "hidden";
+  } else {
+    // Enable scrolling
+    setTimeout(() => {
+      wrapper.style.overflow = "";
+    }, 600);
   }
 }
-
-const day = dates[index].toLocaleString("en-US", { day: "numeric", timeZone: "Europe/Prague" });
-const month = display[dates[index].getMonth()];
-const str = `${month} ${day}`;
-const dateElement = document.getElementById("date");
-if (dateElement) {
-  dateElement.innerHTML = str;
-}
-
